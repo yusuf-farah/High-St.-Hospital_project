53a5643766b45f979b66f5e1bb80e5d967274e0b
yusuf-farah <yfarah1577@gmail.com>
2017-10-12 17:04:30 -0400

    surgeon class created

--- High St. Hospital_project/Hospital employee.cs
+++ High St. Hospital_project/Hospital employee.cs
@@ -7,6 +7,36 @@ using System.Threading.Tasks;
 namespace High_St.Hospital_project
 {
     class Hospital_employee
-    {
+    {   // fields 
+        protected string name;
+        protected int employeenumber;
+        //properties
+        public string Name
+        {
+            get { return this. name; }
+            set { this.name = value; }
+        }
+       public int Employeenumber
+        {
+            get { return this.employeenumber; }
+            set { this.employeenumber = value; }
+        }
+        public Hospital_employee() //
+        {
+        }
+        // bellow code is method
+        public Hospital_employee(string name, int employeenumber)
+        {   
+            this.name= name;
+            this.employeenumber = employeenumber;
+        
+        }
+        
+
+
+
+
+
+
     }
 }
--- High St. Hospital_project/doctor.cs
+++ High St. Hospital_project/doctor.cs
@@ -6,34 +6,27 @@ using System.Threading.Tasks;
 
 namespace High_St.Hospital_project
 {
-    class doctor
+    class doctor:Hospital_employee
     {
-        protected string doctorname;
+       
         protected string specialtyarea;
-        protected int doctoremployeeid;
         
-        public string Doctorname
-        {
-            get { return this. doctorname; }
-            set { this.doctorname = value; }
-        }   
+        
+       
 
         public string Specialtyarea
         {
             get { return this. specialtyarea; }
             set { this.specialtyarea = value; }
         }
-        public int Doctoremployeeid
-        {
-            get { return this. doctoremployeeid; }
-            set { this.doctoremployeeid = value; }
-        }
+      
+        
 
-        public doctor(string doctorname, string  specialtyarea, int doctoremployeeid )
+     public doctor(string doctorname, string  specialtyarea, int doctoremployeeid )
         {
-            this.doctorname = doctorname;
+            this.name = name;
             this.specialtyarea = specialtyarea;
-            this.doctoremployeeid = Doctoremployeeid;
+            this.employeenumber = employeenumber;
         }
 
            
