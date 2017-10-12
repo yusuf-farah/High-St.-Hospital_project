ccaa218901098bc258dd9f1ea6f629a94ab6548c
yusuf-farah <yfarah1577@gmail.com>
2017-10-12 15:49:58 -0400

    added new code

---
+++ High St. Hospital_project/doctor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace High_St.Hospital_project
+{
+    class doctor
+    {
+        protected string doctorname;
+        protected string specialtyarea;
+        protected int doctoremployeeid;
+        
+        public string Doctorname
+        {
+            get { return this. doctorname; }
+            set { this.doctorname = value; }
+        }   
+
+        public string Specialtyarea
+        {
+            get { return this. specialtyarea; }
+            set { this.specialtyarea = value; }
+        }
+        public int Doctoremployeeid
+        {
+            get { return this. doctoremployeeid; }
+            set { this.doctoremployeeid = value; }
+        }
+
+        public doctor(string doctorname, string  specialtyarea, int doctoremployeeid )
+        {
+            this.doctorname = doctorname;
+            this.specialtyarea = specialtyarea;
+            this.doctoremployeeid = Doctoremployeeid;
+        }
+
+           
+
+    }
+}
---
+++ High St. Hospital_project/receptionist.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace High_St.Hospital_project
+{
+    class receptionist:Hospital_employee
+    {
+        // fields
+       
+        
+        public string department;
+        protected bool  phone;
+       
+        
+        public string Department
+        {
+            get { return this.department; }
+            set { this.department = value; }
+        }
+        public bool Phone
+        {
+            get { return this.phone; }
+            set { this.phone = value; }
+        }
+
+            public receptionist(string name, int employeenumber, string department, bool phone)
+        {
+                this.name = name;
+                this.employeenumber = employeenumber;
+                this.department = department;
+                this.phone = phone; 
+        }
+
+        
+    }
+}
